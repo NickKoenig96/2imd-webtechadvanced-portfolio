55f68421305605cd8241befe5eac7a37cfb78995
Nick Koenig <nick.koenig.be@gmail.com>
2021-03-10 13:26:02 +0100

    added weather api

--- lab4/app.js
+++ lab4/app.js
@@ -18,7 +18,28 @@ class App {
     
     }
 
+    getWeather(lat , lng){
+        let key = 'e7dfca2a9d5e817941782d9872085b04';
+        //let url = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}&units=metric`;
+        let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${key}&units=metric`;
+        console.log(url);
+        fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json)
 
+            let temperature = json.current.temp;
+        console.log(temperature);
+        
+        })
+
+        
+
+      
+
+    }
 
 
 }
