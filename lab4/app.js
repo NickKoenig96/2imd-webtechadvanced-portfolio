ac33e85894f4697513184bce887d96c700ea75fb
Nick Koenig <nick.koenig.be@gmail.com>
2021-03-10 13:33:45 +0100

    added innerHTML

--- lab4/app.js
+++ lab4/app.js
@@ -6,39 +6,41 @@ class App {
 
 
     Start() {
-        
-        navigator.geolocation.getCurrentPosition((position)=> {
-            let lat =position.coords.latitude;
+
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
             let lng = position.coords.longitude;
 
-            console.log(lat+lng);
+            console.log(lat + lng);
 
             this.getWeather(lat, lng);
         });
-    
+
     }
 
-    getWeather(lat , lng){
+    getWeather(lat, lng) {
         let key = 'e7dfca2a9d5e817941782d9872085b04';
         //let url = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}&units=metric`;
         let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${key}&units=metric`;
         console.log(url);
         fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((json) => {
-            console.log(json)
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                console.log(json)
 
-            let temperature = json.current.temp;
-        console.log(temperature);
-        
-        })
+                let temperature = json.current.temp;
+                console.log(temperature);
 
-        
+                this.showAd(temperature);
 
-      
+            })
 
+    }
+
+    showAd(temperature){
+        document.querySelector('#temp').innerHTML = temperature;
     }
 
 
